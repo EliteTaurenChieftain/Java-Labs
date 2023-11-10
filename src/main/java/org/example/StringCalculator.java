66a9cd1f224f22a4a16f50495bf6033fc792de04
EliteTaurenChieftain <murzabogdan75@gmail.com>
2023-11-10 20:13:06 +0300

    commit 2

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,30 +1,23 @@
 package org.example;
 
 public class  StringCalculator {
-    public static int add(String numbers){
-        if(numbers.isEmpty()){
+    public static int add(String numbers) {
+        if (numbers.isEmpty()) {
             return 0;
         }
+        int sum = 0;
         String[] numberArray = numbers.split(",");
-        if(numberArray.length == 1){
-            return parseNum(numberArray[0]);
-        } else if(numberArray.length ==2){
-            int num1 = parseNum(numberArray[0]);
-            int num2 = parseNum(numberArray[1]);
-            return num1 + num2;
-        } else {
-            throw new IllegalArgumentException("Input can't contain more than two numbers");
+        for (String num : numberArray) {
+            sum += parseNum(num);
         }
+        return sum;
     }
+
     public static int parseNum(String num) {
         return Integer.parseInt(num);
     }
     public static void main(String[] args) {
-        int result1 = StringCalculator.add("");         // Returns 0
-        int result2 = StringCalculator.add("1");        // Returns 1
-        int result3 = StringCalculator.add("1,2");      // Returns 3
-        System.out.println(result1);
-        System.out.println(result2);
+        int result3 = StringCalculator.add("1,2,3,4");      // Returns 3
         System.out.println(result3);
     }
 }
