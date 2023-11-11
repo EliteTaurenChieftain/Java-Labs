f7075f9f484841e0ae457cbab94ceea80f2453f1
EliteTaurenChieftain <murzabogdan75@gmail.com>
2023-11-11 12:51:33 +0300

    commit 8-9

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -2,61 +2,58 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
-public class StringCalculator {
+public class  StringCalculator {
     public static int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
 
-        String delimiter=",";
+        String delimiter = ",";
         if (numbers.startsWith("//")) {
             int startIndex = numbers.indexOf("//");
             int endIndex = numbers.indexOf("\n");
-            if (startIndex != -1 && endIndex != -1 && endIndex > startIndex) {
+            if (startIndex != -1 && endIndex != -1 && startIndex < endIndex) {
                 delimiter = numbers.substring(startIndex + 2, endIndex);
                 numbers = numbers.substring(endIndex + 1);
             }
-            if (delimiter.startsWith("[")) {
-                int startIndek = delimiter.indexOf("[");
-                int endIndek = delimiter.indexOf("]");
-                if (startIndek != -1 && endIndek != -1 && endIndek > startIndek) {
-                    delimiter = delimiter.substring(startIndek + 1, endIndek);
+        }
+
+        List<Integer> negativeNumbers = new ArrayList<>();
+        int sum = 0;
+        String[] numberArray = numbers.split("[" + delimiter + "]|\\n|,");
+        for (String num : numberArray) {
+            if (!num.isEmpty()) {
+                int parsedNum = parseNum(num);
+                if (parsedNum < 0) {
+                    negativeNumbers.add(parsedNum);
+                }
+                if (parsedNum <= 1000) {
+                    sum += parsedNum;
                 }
             }
         }
 
-
-        int sum = 0;
-        List<Integer> negativeNumbers = new ArrayList<>();
-        String[] numberArray = numbers.split(Pattern.quote(delimiter) + "|\\n|,");
-        for (String num : numberArray) {
-            if (parseNum(num) < 0) {
-                negativeNumbers.add(parseNum(num));
-            }
-            if (parseNum(num) < 1000) {
-                sum += parseNum(num);
-            }
-        }
         if (!negativeNumbers.isEmpty()) {
-            try {
-                throw new Exception("Negative number entered" + negativeNumbers);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            throw new IllegalArgumentException("Negatives not allowed: " + negativeNumbers);
         }
+
         return sum;
     }
-
-
 
     public static int parseNum(String num) {
         return Integer.parseInt(num);
     }
 
     public static void main(String[] args) {
-        int result4 = StringCalculator.add("//.\n-1.-2.-3.-8,-2\n1");
-        System.out.println(result4);
+
+         int result5 = StringCalculator.add("");
+
+
+
+        System.out.println(result5);
+
+
+
     }
 }
